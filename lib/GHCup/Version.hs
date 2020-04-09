9602db31abe8d647ecf26cde3361311aef6df9e1
Julian Ospald <hasufell@posteo.de>
2020-04-09 20:36:03 +0200

    Bump version to 0.1.0

--- lib/GHCup/Version.hs
+++ lib/GHCup/Version.hs
@@ -9,8 +9,9 @@ import           Data.Versions
 import           URI.ByteString
 import           URI.ByteString.QQ
 
+-- | This reflects the API version of the JSON.
 ghcupURL :: URI
 ghcupURL = [uri|https://www.haskell.org/ghcup/data/ghcup-0.0.1.json|]
 
 ghcUpVer :: PVP
-ghcUpVer = [pver|0.0.1|]
+ghcUpVer = [pver|0.1.0|]
