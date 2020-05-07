1207c7b3dd52327a409aa3be48e6271f6b2aa633
Julian Ospald <hasufell@posteo.de>
2020-05-07 18:28:27 +0200

    Revert "Remove travis"
    This reverts commit 5e35a91ef09f39b35caf23074eff841ebc3649ad.

---
+++ .travis/build.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+set -ex
+
+## install ghc via old ghcup
+
+mkdir -p ~/.ghcup/bin
+curl https://gitlab.haskell.org/haskell/ghcup/raw/master/ghcup > ~/.ghcup/bin/ghcup
+chmod +x ~/.ghcup/bin/ghcup
+
+export PATH="$HOME/.ghcup/bin:$PATH"
+
+ghcup install 8.8.3
+ghcup install-cabal 3.2.0.0
+ghcup set 8.8.3
+
+
+## install ghcup
+
+cabal update
+cabal build
+cp "$(cabal new-exec --verbose=0 --offline sh -- -c 'command -v ghcup')" "./${ARTIFACT}"
