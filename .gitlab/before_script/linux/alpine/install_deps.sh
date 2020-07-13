b52fa23ca26d936684c836c9e6a4994a3be46484
Julian Ospald <hasufell@posteo.de>
2020-07-13 22:34:38 +0200

    Update alpine install-deps

--- .gitlab/before_script/linux/alpine/install_deps.sh
+++ .gitlab/before_script/linux/alpine/install_deps.sh
@@ -23,26 +23,13 @@ ln -sf libncursesw.so.6 /usr/lib/libtinfow.so.6
 ln -sf libtinfow.so.6 /usr/lib/libtinfow.so
 
 if [ "${BIT}" = "32" ] ; then
-	curl -sSfL https://downloads.haskell.org/~ghcup/0.1.5/i386-linux-ghcup-0.1.5 > ./ghcup-bin
+	curl -sSfL https://downloads.haskell.org/ghcup/i386-linux-ghcup > ./ghcup-bin
 else
-	curl -sSfL https://downloads.haskell.org/~ghcup/0.1.5/x86_64-linux-ghcup-0.1.5 > ./ghcup-bin
+	curl -sSfL https://downloads.haskell.org/ghcup/x86_64-linux-ghcup > ./ghcup-bin
 fi
 chmod +x ghcup-bin
-./ghcup-bin upgrade
 ./ghcup-bin install ${GHC_VERSION}
-# ./ghcup-bin install-cabal ${CABAL_VERSION}
-# install cabal-3.2.0.0
-if [ "${BIT}" = "32" ] ; then
-	curl -sSfL -o cabal-install-3.2.0.0-i386-alpine-linux-musl.tar.xz 'https://hasufell.de/d/d3e215db133e4fcaa61e/files/?p=/cabal-install-3.2.0.0-i386-alpine-linux-musl.tar.xz&dl=1'
-	tar xf cabal-install-3.2.0.0-i386-alpine-linux-musl.tar.xz
-	cp cabal-install-3.2.0.0-i386-alpine-linux-musl "${GHCUP_INSTALL_BASE_PREFIX}/.ghcup/bin/cabal"
-else
-	curl -sSfL -o cabal-install-3.2.0.0-x86_64-alpine-linux-musl.tar.xz 'https://hasufell.de/d/d3e215db133e4fcaa61e/files/?p=/cabal-install-3.2.0.0-x86_64-alpine-linux-musl.tar.xz&dl=1'
-	tar xf cabal-install-3.2.0.0-x86_64-alpine-linux-musl.tar.xz
-	cp cabal-install-3.2.0.0-x86_64-alpine-linux-musl "${GHCUP_INSTALL_BASE_PREFIX}/.ghcup/bin/cabal"
-fi
-chmod +x "${GHCUP_INSTALL_BASE_PREFIX}/.ghcup/bin/cabal"
-
+./ghcup-bin install-cabal ${CABAL_VERSION}
 
 # utils
 apk add --no-cache \
