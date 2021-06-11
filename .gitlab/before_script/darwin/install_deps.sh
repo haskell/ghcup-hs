a13a5e5d2062327720b438cf72a7a39be604378f
Julian Ospald <hasufell@posteo.de>
2021-06-11 11:52:20 +0200

    Update darwin aarch64 install deps

--- .gitlab/before_script/darwin/install_deps.sh
+++ .gitlab/before_script/darwin/install_deps.sh
@@ -8,12 +8,13 @@ mkdir -p "${TMPDIR}"
 
 if [ $ARCH = 'ARM64' ] ; then
 	curl -sSfL https://downloads.haskell.org/~ghcup/0.1.15.1/aarch64-apple-darwin-ghcup-0.1.15.1 > ./ghcup-bin
+	chmod +x ghcup-bin
 else
 	curl -sSfL https://downloads.haskell.org/~ghcup/x86_64-apple-darwin-ghcup > ./ghcup-bin
+	chmod +x ghcup-bin
+	./ghcup-bin upgrade -i -f
 fi
-chmod +x ghcup-bin
 
-./ghcup-bin upgrade -i -f
 ./ghcup-bin install ${GHC_VERSION}
 ./ghcup-bin set ${GHC_VERSION}
 ./ghcup-bin install-cabal ${CABAL_VERSION}
