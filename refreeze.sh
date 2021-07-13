8573c99088684870b5fa3a380555034ce4d8d690
Julian Ospald <hasufell@posteo.de>
2021-07-13 15:17:12 +0200

    Update freeze stuff

---
+++ refreeze.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -xue
+
+rm -f cabal.*.project
+rm -f cabal.*.project.freeze
+
+for ghc_ver in "$@" ; do
+	# shellcheck disable=SC3060
+	project_file=cabal.ghc${ghc_ver//./}.project
+
+	cp cabal.project "${project_file}"
+	case "$(uname -s)" in
+        MSYS*|MINGW*)
+			cabal freeze --project-file="${project_file}" -w "ghc-${ghc_ver}"
+			;;
+		*)
+			cabal freeze --project-file="${project_file}" -w "ghc-${ghc_ver}" -ftui -finternal-downloader
+			;;
+	esac
+
+
+	echo "" >> "${project_file}"
+	echo "with-compiler: ghc-${ghc_ver}" >> "${project_file}"
+
+	sed -i -e '/ghcup/d' "${project_file}".freeze
+done
