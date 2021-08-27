20bcb26e3d7d287d46ab2204c726656a300489bf
Arjun Kathuria <arjun.kathuria8@gmail.com>
2021-08-27 13:05:54 +0530

    Adds the --force option in install commands

--- app/ghcup/Main.hs
+++ app/ghcup/Main.hs
@@ -141,6 +141,7 @@ data InstallOptions = InstallOptions
   , instBindist  :: Maybe URI
   , instSet      :: Bool
   , isolateDir   :: Maybe FilePath
+  , forceInstall :: Bool
   }
 
 data SetCommand = SetGHC SetOptions
@@ -602,7 +603,7 @@ Examples:
 
 installOpts :: Maybe Tool -> Parser InstallOptions
 installOpts tool =
-  (\p (u, v) b is -> InstallOptions v p u b is)
+  (\p (u, v) b is f -> InstallOptions v p u b is f)
     <$> optional
           (option
             (eitherReader platformParser)
@@ -640,6 +641,9 @@ installOpts tool =
            <> help "install in an isolated dir instead of the default one"
            )
           )
+    <*> switch
+          (short 'f' <> long "force" <> help "Force install")
+          
 
 
 setParser :: Parser (Either SetCommand SetOptions)
