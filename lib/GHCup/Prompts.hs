2bd5a8fe1ac51a3a2b0801e18cf9216e3c63619b
Arjun Kathuria <arjun.kathuria8@gmail.com>
2022-07-10 09:45:39 +0530

    Removes redundant `putPrompt` function from Prompts module.

--- lib/GHCup/Prompts.hs
+++ lib/GHCup/Prompts.hs
@@ -12,19 +12,16 @@ import Control.Monad.Reader
 import qualified Data.Text.IO as TIO
 import GHCup.Prelude.Logger
 import GHCup.Types.Optics
-
-putPrompt :: (HasLog env, MonadReader env m, MonadIO m)
-          => PromptQuestion
-          -> m ()
-putPrompt = logInfo
+import GHCup.Types (PromptQuestion, PromptResponse(..))
 
 getUserPromptResponse :: ( HasLog env
                          , MonadReader env m
                          , MonadIO m)
                       => PromptQuestion
                       -> m PromptResponse
+
 getUserPromptResponse prompt = do
-  putPrompt prompt
+  logInfo prompt
   resp <- liftIO TIO.getLine
   if resp `elem` ["YES", "yes", "y", "Y"]
     then pure PromptYes
