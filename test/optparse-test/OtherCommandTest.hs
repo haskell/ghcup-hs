16c7ecabe2b82cb42180b104f30df51f0efbe5bc
Lei Zhu <julytreee@gmail.com>
2023-07-22 17:20:42 +0800

    nuke test

--- test/optparse-test/OtherCommandTest.hs
+++ test/optparse-test/OtherCommandTest.hs
@@ -17,8 +17,15 @@ otherCommandTests = testGroup "other command"
   , testCase "tool-requirements -r" $ do
       ToolRequirements opt <- parseWith ["tool-requirements", "--raw-format"]
       liftIO $ tlrRaw opt @?= True
+  , testCase "nuke" $ do
+      res <- parseWith ["nuke"]
+      liftIO $ assertBool "nuke parse failed" (isNuke res)
   ]
 
 isDInfo :: Command -> Bool
 isDInfo DInfo = True
 isDInfo _     = False
+
+isNuke :: Command -> Bool
+isNuke Nuke = True
+isNuke _    = False
