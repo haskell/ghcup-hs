b2d276cd4335cd6a66f4492868e2f695977dbb3b
Julian Ospald <hasufell@posteo.de>
2023-09-01 13:14:24 +0800

    Add WT keys

--- .github/workflows/test-sigs.sh
+++ .github/workflows/test-sigs.sh
@@ -13,7 +13,7 @@ get_key() {
 }
 
 # verify signature
-keys=( 7D1E8AFD1D4A16D71FADA2F2CCC85C0E40C06A8C )
+keys=( 7D1E8AFD1D4A16D71FADA2F2CCC85C0E40C06A8C FE5AB6C91FEA597C3B31180B73EDE9E8CFBAEF01 88B57FCF7DB53B4DB3BFA4B1588764FBE22D19C4 )
 for key in "${keys[@]}" ; do
 	get_key "${key}" keys.openpgp.org || get_key "${key}" keyserver.ubuntu.com
 done
