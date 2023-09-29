ac43fb74c85ad6ad87ae8060c48ef5877e597f0c
Hécate Moonlight <hecate+github@glitchbra.in>
2023-09-29 02:16:54 +0200

    Add Hécate's PGP key

--- .github/workflows/test-sigs.sh
+++ .github/workflows/test-sigs.sh
@@ -17,6 +17,7 @@ keys=(
     7D1E8AFD1D4A16D71FADA2F2CCC85C0E40C06A8C # Julian Ospald <maerwald@hasufell.de>
     FFEB7CE81E16A36B3E2DED6F2DE04D4E97DB64AD # Ben Gamari <ben@well-typed.com>
     88B57FCF7DB53B4DB3BFA4B1588764FBE22D19C4 # Zubin Duggal <zubin@well-typed.com>
+    EAF2A9A722C0C96F2B431CA511AAD8CEDEE0CAEF # Hécate <hecate@glitchbra.in>
 )
 
 for key in "${keys[@]}" ; do
