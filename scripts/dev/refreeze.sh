a4290909b4bf8594af6ebc6d4895547b64f1df33
Julian Ospald <hasufell@posteo.de>
2024-03-16 17:56:01 +0800

    Use platform specific project files

--- scripts/dev/refreeze.sh
+++ scripts/dev/refreeze.sh
@@ -9,20 +9,33 @@ for ghc_ver in "$@" ; do
 	# shellcheck disable=SC3060
 	project_file=cabal.ghc${ghc_ver//./}.project
 
-	cp cabal.project "${project_file}"
 	case "$(uname -s)" in
         MSYS*|MINGW*)
-			cabal freeze --project-file="${project_file}" -w "ghc-${ghc_ver}" -ftui
+	        # shellcheck disable=SC3060
+	        project_file_os=cabal.ghc${ghc_ver//./}.Win32.project
+	        cp cabal.project "${project_file_os}"
+			cabal freeze --project-file="${project_file_os}" -w "ghc-${ghc_ver}" -ftui
 			;;
 		*)
-			cabal freeze --project-file="${project_file}" -w "ghc-${ghc_ver}" -ftui -finternal-downloader
+	        # shellcheck disable=SC3060
+	        project_file_os=cabal.ghc${ghc_ver//./}.Unix.project
+	        cp cabal.project "${project_file_os}"
+			cabal freeze --project-file="${project_file_os}" -w "ghc-${ghc_ver}" -ftui -finternal-downloader
 			;;
 	esac
 
 
-	echo "" >> "${project_file}"
-	echo "with-compiler: ghc-${ghc_ver}" >> "${project_file}"
+	sed -i -e '/ghcup/d' "${project_file_os}".freeze
 
-	sed -i -e '/ghcup/d' "${project_file}".freeze
+    cat <<EOF > "${project_file}" || die
+if os(mingw32)
+  import: cabal.ghc${ghc_ver//./}.Win32.project
+  import: cabal.ghc${ghc_ver//./}.Win32.project.freeze
+else
+  import: cabal.ghc${ghc_ver//./}.Unix.project
+  import: cabal.ghc${ghc_ver//./}.Unix.project.freeze
+
+with-compiler: ghc-${ghc_ver}
+EOF
 done
 
