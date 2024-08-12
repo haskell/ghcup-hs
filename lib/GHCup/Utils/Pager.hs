f88ddea3a5489a51eed07cc26e6414edbd67ec3b
Julian Ospald <hasufell@posteo.de>
2024-08-12 21:55:34 +0800

    Overhaul sendToPager

--- lib/GHCup/Utils/Pager.hs
+++ lib/GHCup/Utils/Pager.hs
@@ -31,21 +31,20 @@ getPager = do
         asum $ fmap findExecutable pagers
 
 sendToPager :: FilePath -> [Text] -> IO (Either IOException ())
-sendToPager pager text = do
-  (Just stdinH, _, Just stderrH, ph) <-
-    createProcess
-      $ (shell pager) { std_in = CreatePipe
-                      , std_err = CreatePipe
-                      }
-  try @IOException (forM_ text $ T.hPutStrLn stdinH) >>= \case
-    Left e -> pure $ Left e
-    Right _ -> do
-      hClose stdinH
-      exitCode <- waitForProcess ph
-      case exitCode of
-        ExitFailure i ->
-          do errContents <- hGetContents stderrH
-             fail (unlines [mappend "Pager exited with exit code " (show i)
-                           ,errContents])
-        _ -> pure $ Right ()
+sendToPager pager text = try @IOException
+    $ withCreateProcess (shell pager) { std_in = CreatePipe
+                                      , std_err = CreatePipe
+                                      , std_out = Inherit
+                                      , delegate_ctlc = True
+                                      }
+    $ \(Just stdinH) _ (Just stderrH) ph -> do
+        forM_ text $ T.hPutStrLn stdinH
+        hClose stdinH
+        exitCode <- waitForProcess ph
+        case exitCode of
+          ExitFailure i ->
+            do errContents <- hGetContents stderrH
+               fail (unlines [mappend "Pager exited with exit code " (show i)
+                             ,errContents])
+          _ -> pure ()
 
