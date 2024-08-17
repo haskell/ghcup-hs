8588cf816921b7371041340a57b5d3175c5f7221
Julian Ospald <hasufell@posteo.de>
2024-08-17 18:21:43 +0800

    Toto

--- lib/GHCup/Utils/Pager.hs
+++ lib/GHCup/Utils/Pager.hs
@@ -5,7 +5,6 @@
 
 module GHCup.Utils.Pager where
 
-import GHCup.Prelude
 import System.Environment
 import GHCup.Utils.Dirs (findExecutable)
 import Data.Foldable (asum)
@@ -14,7 +13,7 @@ import System.Exit
 import System.IO
 import Data.Text (Text)
 import qualified Data.Text.IO as T
-import Control.Monad (forM_)
+import Control.Monad (forM_, (<=<))
 import Control.Exception (IOException, try)
 import GHCup.Utils.Output
 import qualified Data.Text as T
@@ -26,11 +25,13 @@ getPager = do
     Just r  -> pure $ Just r
     Nothing -> lookupEnv "PAGER" >>= \case
       Just r' -> pure $ Just r'
-      Nothing -> do
-        let pagers
-              | isWindows = ["most.exe", "more.exe", "less.exe"]
-              | otherwise = ["most", "more", "less"]
-        asum $ fmap findExecutable pagers
+      Nothing ->
+        let pagers = ["most", "more", "less"]
+        in fmap (either (const Nothing) Just)
+           . try @IOException
+           . asum
+           . fmap (maybe (fail "could not find") pure <=< findExecutable)
+           $ pagers
 
 sendToPager :: FilePath -> [Text] -> IO (Either IOException ())
 sendToPager pager text = try @IOException
