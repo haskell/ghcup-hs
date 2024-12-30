haskell/ghcup-hs

commit b84eeea67fe3cbe4c7ed78c29d002bdd6c8ffaba

Beef up parseUrlSource

lib/GHCup/Utils/Parsers.hs
@@ -16,7 +16,8 @@ import           GHCup.List
 import           GHCup.Utils
 import           GHCup.Prelude
 import           GHCup.Prelude.Logger
-import           GHCup.Prelude.MegaParsec
+import           GHCup.Prelude.Attoparsec as AP
+import           GHCup.Prelude.MegaParsec as MP
 
 import           Control.Applicative ((<|>), Alternative(..))
 import           Control.Monad (when)
@@ -26,12 +27,13 @@ import           Control.Monad.Fail             ( MonadFail )
 #endif
 import           Control.Monad.Reader
 import           Data.Aeson
+import qualified Data.Attoparsec.ByteString as AP
 #if MIN_VERSION_aeson(2,0,0)
 #else
 import qualified Data.HashMap.Strict as KM
 #endif
 import           Data.Bifunctor
-import           Data.Char
+import           Data.Char               as C
 import           Data.Either
 import           Data.Functor
 import           Data.List                      ( sort, sortBy )
@@ -382,20 +384,34 @@ fromVersion' (SetToolTag t') tool =
 
 
 parseUrlSource :: String -> Either String [NewURLSource]
-parseUrlSource s = (fromURLSource <$> parseUrlSource' s) <|> ((:[]) <$> parseNewUrlSource s)
+parseUrlSource s = (fromURLSource <$> parseUrlSource' s) <|> ((:[]) <$> parseNewUrlSource s) <|> (parseNewUrlSources s)
 
 parseUrlSource' :: String -> Either String URLSource
 parseUrlSource' "GHCupURL" = pure GHCupURL
 parseUrlSource' "StackSetupURL" = pure StackSetupURL
 parseUrlSource' s' = (eitherDecode . LE.encodeUtf8 . LT.pack $ s')
-            <|> (fmap (OwnSource . (:[]) . Right) . first show . parseURI .UTF8.fromString $ s')
+            <|> (fmap (OwnSource . (:[]) . Right) . first show . parseURI . UTF8.fromString $ s')
 
 parseNewUrlSource :: String -> Either String NewURLSource
 parseNewUrlSource "GHCupURL" = pure NewGHCupURL
 parseNewUrlSource "StackSetupURL" = pure NewStackSetupURL
 parseNewUrlSource s' = (fmap NewChannelAlias . parseChannelAlias $ s')
             <|> (eitherDecode . LE.encodeUtf8 . LT.pack $ s')
-            <|> (fmap NewURI . first show . parseURI .UTF8.fromString $ s')
+            <|> (fmap NewURI . first show . parseURI . UTF8.fromString $ s')
+
+parseNewUrlSources :: String -> Either String [NewURLSource]
+parseNewUrlSources s = case AP.parseOnly
+                              (AP.parseList' <* AP.skipSpaces <* AP.endOfInput)
+                              (UTF8.fromString s) of
+  Right bs ->
+    forM bs $ \b -> AP.parseOnly (parse <* AP.skipSpaces <* AP.endOfInput) b
+  Left  e -> Left e
+ where
+  parse :: AP.Parser NewURLSource
+  parse = (NewGHCupURL <$ AP.string "GHCupURL")
+      <|> (NewStackSetupURL <$ AP.string "StackSetupURL")
+      <|> AP.choice ((\x -> AP.string (UTF8.fromString . T.unpack . channelAliasText $ x) $> NewChannelAlias x) <$> ([minBound..maxBound] :: [ChannelAlias]))
+      <|> (NewURI <$> parseURI')
 
 parseChannelAlias :: String -> Either String ChannelAlias
 parseChannelAlias s =

ghcup.cabal
@@ -82,6 +82,7 @@ common app-common-depends
     , aeson                  >=1.4
     , aeson-pretty           ^>=0.8.8
     , async                  ^>=2.2.3
+    , attoparsec             ^>= 0.14
     , base                   >=4.12     && <5
     , bytestring             >=0.10     && <0.13
     , containers             ^>=0.6
@@ -136,6 +137,7 @@ library
     GHCup.Platform
     GHCup.PlanJson
     GHCup.Prelude
+    GHCup.Prelude.Attoparsec
     GHCup.Prelude.File
     GHCup.Prelude.File.Search
     GHCup.Prelude.Internal
@@ -191,6 +193,7 @@ library
   build-depends:
     , aeson                 >=1.4
     , async                 >=0.8        && <2.3
+    , attoparsec            ^>= 0.14
     , base                  >=4.12       && <5
     , base16-bytestring     >=0.1.1.6    && <1.1
     , binary                ^>=0.8.6.0

lib/GHCup/Prelude/Attoparsec.hs
@@ -1,0 +1,50 @@
+{-# LANGUAGE CPP                  #-}
+{-# LANGUAGE OverloadedStrings    #-}
+
+{-|
+Module      : GHCup.Utils.Attoparsec
+Description : Attoparsec utilities
+Copyright   : (c) Julian Ospald, 2024
+License     : LGPL-3.0
+Maintainer  : hasufell@hasufell.de
+Stability   : experimental
+Portability : portable
+-}
+module GHCup.Prelude.Attoparsec where
+
+import           Control.Applicative
+#if !MIN_VERSION_base(4,13,0)
+import           Control.Monad.Fail             ( MonadFail )
+#endif
+import           Data.ByteString (ByteString)
+import           Data.ByteString.Internal (w2c)
+import           Data.Functor
+
+import           Data.Attoparsec.ByteString as AP
+
+
+
+parseList :: Parser a -> Parser [a]
+parseList p = skipSpaces *> string "[" *> skipSpaces *> loop
+ where
+  loop = (:) <$> p <*> (     (skipSpaces *> string "," *> skipSpaces *> loop)
+                         <|> ([] <$ skipSpaces <* string "]")
+                       )
+
+parseList' :: Parser [ByteString]
+parseList' = skipSpaces *> string "[" *> skipSpaces *> loop
+ where
+  loop = (:) <$> AP.takeWhile (\c -> w2c c `notElem` [',',']'] && not (isSpace $ w2c c)) <*> ((skipSpaces *> string "," *> skipSpaces *> loop)
+                         <|> ([] <$ skipSpaces <* string "]")
+                       )
+
+skipSpace :: Parser ()
+skipSpace = void $ satisfy (isSpace . w2c)
+
+skipSpaces :: Parser ()
+skipSpaces = void $ many skipSpace
+
+isSpace :: Char -> Bool
+isSpace c = (c == ' ') || ('\t' <= c && c <= '\r')
+{-# INLINE isSpace #-}
+

lib/GHCup/Prelude/MegaParsec.hs
@@ -130,6 +130,9 @@ skip f = void $ MP.satisfy f
 skipSpace :: MP.Parsec Void Text ()
 skipSpace = void $ MP.satisfy isSpace
 
+skipSpaces :: MP.Parsec Void Text ()
+skipSpaces = void $ many skipSpace
+
 isSpace :: Char -> Bool
 isSpace c = (c == ' ') || ('\t' <= c && c <= '\r')
 {-# INLINE isSpace #-}

lib/GHCup/Utils/URI.hs
@@ -15,11 +15,13 @@ URI handling.
 -}
 module GHCup.Utils.URI where
 
+import           Data.Bifunctor (first)
+import           Control.Applicative
+import           Data.Attoparsec.ByteString
 import           Data.ByteString
 import           URI.ByteString hiding (parseURI)
 import           System.URI.File
 
-import qualified URI.ByteString                as URI
 
 
     -----------
@@ -28,22 +30,25 @@ import qualified URI.ByteString                as URI
 
 
 parseURI :: ByteString -> Either URIParseError (URIRef Absolute)
-parseURI bs = case parseFile bs of
-                Left _ -> case URI.parseURI strictURIParserOptions bs of
-                            Right (URI { uriScheme = (Scheme "file") }) ->
+parseURI = first OtherError . parseOnly parseURI'
+
+parseURI' :: Parser (URIRef Absolute)
+parseURI' = do
+  ref <- (Right <$> parseFile) <|> (Left <$> uriParser laxURIParserOptions)
+  case ref of
+    Left (URI { uriScheme = (Scheme "file") }) ->
 #if defined(IS_WINDOWS)
-                              Left (OtherError "Invalid file URI. File URIs must be absolute (start with a drive letter or UNC path) and not contain backslashes.")
+      fail "Invalid file URI. File URIs must be absolute (start with a drive letter or UNC path) and not contain backslashes."
 #else
-                              Left (OtherError "Invalid file URI. File URIs must be absolute.")
+      fail "Invalid file URI. File URIs must be absolute."
 #endif
-                            o -> o
-                Right (FileURI (Just _) _) -> Left $ OtherError "File URIs with auth part are not supported!"
-                Right (FileURI _ fp) -> Right $ URI (Scheme "file") Nothing fp (Query []) Nothing
+    Left o -> pure o
+    Right (FileURI (Just _) _) -> fail "File URIs with auth part are not supported!"
+    Right (FileURI _ fp) -> pure $ URI (Scheme "file") Nothing fp (Query []) Nothing
  where
   parseFile
 #if defined(IS_WINDOWS)
-    = parseFileURI ExtendedWindows
+    = fileURIExtendedWindowsP
 #else
-    = parseFileURI ExtendedPosix
+    = fileURIExtendedPosixP
 #endif
-

commit 28ebef01e24038d922c1047b8fe35846ca6f7699

Fix import list (was breaking build)
Fixes: b84eeea67fe3cbe4c7ed78c29d002bdd6c8ffaba

lib/GHCup/Utils/Parsers.hs
@@ -20,7 +20,7 @@ import           GHCup.Prelude.Attoparsec as AP
 import           GHCup.Prelude.MegaParsec as MP
 
 import           Control.Applicative ((<|>), Alternative(..))
-import           Control.Monad (when)
+import           Control.Monad (forM, when)
 import           Control.Exception.Safe
 #if !MIN_VERSION_base(4,13,0)
 import           Control.Monad.Fail             ( MonadFail )