a4cbced8e9ceee94784b77454da564953741e705
Julian Ospald <hasufell@posteo.de>
2026-05-18 01:15:39 +0800

    Use `env`

--- bench/BenchList.hs
+++ bench/BenchList.hs
@@ -14,8 +14,9 @@ import GHCup.Query.GHCupDirs
 import GHCup.Types
 
 import Control.Monad.Reader (runReaderT)
+import Control.Exception (displayException)
 import Data.Maybe
-import Data.Proxy
+import Data.Yaml (decodeFileEither)
 import Data.Variant.Excepts
 import Prelude hiding ( appendFile )
 import Test.Tasty.Bench
@@ -24,20 +25,14 @@ import Test.Tasty.Bench
 benchMark :: Benchmark
 benchMark =
   bgroup "GHCup.Commands.List"
-     [ bench "cross list" $ nfAppIO listVersionsB [NewGHCupInfo crossGHCupInfo]
-     , bench "def list" $ nfAppIO listVersionsB [NewGHCupInfo defGHCupInfo]
-     , bench "nightly list" $ nfAppIO listVersionsB [NewGHCupInfo nightlyGHCupInfo]
+     [ env (readGHCupInfo "data/bench/ghcup-cross-0.1.0.yaml")           (bench "cross"   . nfAppIO listVersionsB)
+     , env (readGHCupInfo "data/bench/ghcup-0.1.0.yaml")                 (bench "def"     . nfAppIO listVersionsB)
+     , env (readGHCupInfo "data/bench/ghcup-nightlies-2025-0.0.7.yaml")  (bench "nightly" . nfAppIO listVersionsB)
      ]
 
 
-defGHCupInfo :: GHCupInfo
-defGHCupInfo = $(embedMetadata (Proxy @GHCupInfo) "data/metadata/ghcup-0.1.0.yaml")
-
-crossGHCupInfo :: GHCupInfo
-crossGHCupInfo = $(embedMetadata (Proxy @GHCupInfo) "data/metadata/ghcup-cross-0.1.0.yaml")
-
-nightlyGHCupInfo :: GHCupInfo
-nightlyGHCupInfo = $(embedMetadata (Proxy @GHCupInfo) "data/bench/ghcup-nightlies-0.0.7.yaml")
+readGHCupInfo :: FilePath -> IO [NewURLSource]
+readGHCupInfo fp = either (fail . displayException) (pure . (:[]) . NewGHCupInfo) =<< decodeFileEither fp
 
 type DownloadErrors = '[ContentLengthError, DigestError, DistroNotFound, DownloadFailed, FileDoesNotExistError, GPGError, JSONError, NoCompatibleArch, NoCompatiblePlatform, NoDownload, GHCup.Errors.ParseError, ProcessError, UnsupportedSetupCombo, StackPlatformDetectError, UnsupportedMetadataFormat]
 
