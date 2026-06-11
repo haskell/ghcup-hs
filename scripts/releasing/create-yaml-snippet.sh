9ad601f9fce86afbb406b99fadd0b84a6b75c380
Julian Ospald <hasufell@posteo.de>
2026-06-11 18:35:51 +0200

    Update create-yaml-snippet

--- scripts/releasing/create-yaml-snippet.sh
+++ scripts/releasing/create-yaml-snippet.sh
@@ -13,56 +13,65 @@ cd "gh-release-artifacts/v${RELEASE}"
 
 cat <<EOF > /dev/stdout
   GHCup:
-    ${RELEASE}:
-      viTags:
-      - Recommended
-      - Latest
-      viChangeLog: https://github.com/haskell/ghcup-hs/blob/master/CHANGELOG.md
-      viSourceDL:
-        dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/ghcup-${RELEASE}-src.tar.gz
-        dlSubdir: ghcup-${RELEASE}
-        dlHash: $(get_sha "ghcup-${RELEASE}-src.tar.gz")
-      viArch:
-        A_64:
-          Linux_UnknownLinux:
-            unknown_versioning: &ghcup-64
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-linux-ghcup-${RELEASE}
-              dlHash: $(get_sha "x86_64-linux-ghcup-${RELEASE}")
-          Darwin:
-            unknown_versioning:
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-apple-darwin-ghcup-${RELEASE}
-              dlHash: $(get_sha "x86_64-apple-darwin-ghcup-${RELEASE}")
-          FreeBSD:
-            unknown_versioning:
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-portbld-freebsd-ghcup-${RELEASE}
-              dlHash: $(get_sha "x86_64-portbld-freebsd-ghcup-${RELEASE}")
-          OpenBSD:
-            unknown_versioning:
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-openbsd-ghcup-${RELEASE}
-              dlHash: $(get_sha "x86_64-openbsd-ghcup-${RELEASE}")
-          Windows:
-            unknown_versioning:
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-mingw64-ghcup-${RELEASE}.exe
-              dlHash: $(get_sha "x86_64-mingw64-ghcup-${RELEASE}.exe")
-          Linux_Alpine:
-            unknown_versioning: *ghcup-64
-        A_32:
-          Linux_UnknownLinux:
-            unknown_versioning: &ghcup-32
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/i386-linux-ghcup-${RELEASE}
-              dlHash: $(get_sha "i386-linux-ghcup-${RELEASE}")
-          Linux_Alpine:
-            unknown_versioning: *ghcup-32
-        A_ARM64:
-          Linux_UnknownLinux:
-            unknown_versioning: &ghcup-aarch64
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/aarch64-linux-ghcup-${RELEASE}
-              dlHash: $(get_sha "aarch64-linux-ghcup-${RELEASE}")
-          Linux_Alpine:
-            unknown_versioning: *ghcup-aarch64
-          Darwin:
-            unknown_versioning:
-              dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/aarch64-apple-darwin-ghcup-${RELEASE}
-              dlHash: $(get_sha "aarch64-apple-darwin-ghcup-${RELEASE}")
+    toolDetails:
+      toolHomepage: "https://www.haskell.org/ghcup/"
+      toolRepository: "https://github.com/haskell/ghcup-hs"
+      toolDescription: "The standard Haskell installer"
+      toolAuthor: "Julian Ospald"
+      toolMaintainer: "Julian Ospald"
+      toolContact: "hasufell@posteo.de"
+      toolLicense: "LGPL-3.0"
+    toolVersions:
+      ${RELEASE}:
+        viTags:
+        - Recommended
+        - Latest
+        viChangeLog: https://github.com/haskell/ghcup-hs/blob/master/CHANGELOG.md
+        viSourceDL:
+          dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/ghcup-${RELEASE}-src.tar.gz
+          dlSubdir: ghcup-${RELEASE}
+          dlHash: $(get_sha "ghcup-${RELEASE}-src.tar.gz")
+        viArch:
+          A_64:
+            Linux_UnknownLinux:
+              unknown_versioning: &ghcup-64
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-linux-ghcup-${RELEASE}
+                dlHash: $(get_sha "x86_64-linux-ghcup-${RELEASE}")
+            Darwin:
+              unknown_versioning:
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-apple-darwin-ghcup-${RELEASE}
+                dlHash: $(get_sha "x86_64-apple-darwin-ghcup-${RELEASE}")
+            FreeBSD:
+              unknown_versioning:
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-portbld-freebsd-ghcup-${RELEASE}
+                dlHash: $(get_sha "x86_64-portbld-freebsd-ghcup-${RELEASE}")
+            OpenBSD:
+              unknown_versioning:
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-openbsd-ghcup-${RELEASE}
+                dlHash: $(get_sha "x86_64-openbsd-ghcup-${RELEASE}")
+            Windows:
+              unknown_versioning:
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/x86_64-mingw64-ghcup-${RELEASE}.exe
+                dlHash: $(get_sha "x86_64-mingw64-ghcup-${RELEASE}.exe")
+            Linux_Alpine:
+              unknown_versioning: *ghcup-64
+          A_32:
+            Linux_UnknownLinux:
+              unknown_versioning: &ghcup-32
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/i386-linux-ghcup-${RELEASE}
+                dlHash: $(get_sha "i386-linux-ghcup-${RELEASE}")
+            Linux_Alpine:
+              unknown_versioning: *ghcup-32
+          A_ARM64:
+            Linux_UnknownLinux:
+              unknown_versioning: &ghcup-aarch64
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/aarch64-linux-ghcup-${RELEASE}
+                dlHash: $(get_sha "aarch64-linux-ghcup-${RELEASE}")
+            Linux_Alpine:
+              unknown_versioning: *ghcup-aarch64
+            Darwin:
+              unknown_versioning:
+                dlUri: https://downloads.haskell.org/~ghcup/${RELEASE}/aarch64-apple-darwin-ghcup-${RELEASE}
+                dlHash: $(get_sha "aarch64-apple-darwin-ghcup-${RELEASE}")
 EOF
 
