1aebc570f0f54e1f28a50925362c4731e82ec972
Julian Ospald <hasufell@posteo.de>
2026-08-19 19:19:09 +0800

    Fix bootstrap without pty

--- scripts/bootstrap/bootstrap-haskell.ps1
+++ scripts/bootstrap/bootstrap-haskell.ps1
@@ -557,7 +557,7 @@ if ($msys2Action -eq 0) {
     Remove-Item -Path "$archivePath" -ErrorAction Continue
 
     Print-Msg -msg 'Processing MSYS2 bash for first time use...'
-    Exec "$Bash" '-lc' 'exit'
+    Start-Process cmd.exe -ArgumentList '/c', "`"$Bash`" -x -lc exit" -Wait
 
     Exec "$env:windir\system32\taskkill.exe" /F /FI "MODULES eq msys-2.0.dll"
 
